e0450220627c589048fb97bf5884d7aacb7293ab
SamsterJam <samsterjam@gmail.com>
2023-10-16 15:48:41 -0400

    Got things working

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    std::cout << "Hello World!";
+    return 0;
+}
