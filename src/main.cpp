fcace77bc88bcc2767e32efe2567d1ddc8e34ae5
SamsterJam <samsterjam@gmail.com>
2023-10-16 16:57:30 -0400

    Black Screen

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,39 @@
 
 using namespace std;
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
 int main(){
-    std::cout << "Hello World!";
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearningOpenGL", NULL, NULL);
+    if(window == NULL){
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
+
+
+    while(!glfwWindowShouldClose(window)){
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     return 0;
 }
